3eb5e81a33106ca7cd1187ddccbf1eeb4b66f9dd
jgilchrist308 <jgilchrist308@gmail.com>
2025-04-02 22:51:09 -0400

    Changed order of functions

--- app/app.py
+++ app/app.py
@@ -8,6 +8,74 @@ import copy
 
 DEBUG = True
 
+def create_data():
+    '''Initializes "chart" - an object generated using the Billboard scraper containing the chart data of the Billboard Hot-100. This data is then processed into points. Number 1 is worth 100, number 2 - 99, etc...
+    Different dictionaries are created containing various groups of data and returned
+
+    Args:
+        None
+
+    Returns:
+        weeklypoints: A dictionary containing the artist and points values from entries that contain no features of any kind
+
+        check_features: A dictionary containing artist and point values from entries that contain features. These point values have not been distributed or weighted yet
+
+        newpoints: A dictionary containing artist and points values with points from top billing features added. None of the featured artists have received credit for their secondary appearances
+
+        halfCreditFeatures: A dictionary containing artists and their half-point totals based on true features. These need to be added manually to the total.
+
+        fullCreditFeatures: A dictionary containing artists and their full-point totals based on co-billings. These also need to be manually added.
+
+        tylerCheck: A dictionary containing chart entries that contain a feature keyword AND a comma. This will filter out songs with more than one featured artist, or Tyler, the Creator and his stupid comma-containing stage name.
+
+    '''
+
+    # Create Chart
+    chart = billboard.ChartData('hot-100')
+
+    # Create Dictionary
+    weeklypoints = {}
+    check_features = {}
+
+    # Loop through chart
+    for i in range(len(chart)):
+        currsong = chart[i]
+
+        # Convert artist name to uppercase for easier parsing
+        currartist = str(currsong.artist).upper()
+
+        # Sort out features
+        if ("&" in currartist) or (" WITH " in currartist.upper()) or ("FEATURING " in currartist.upper()) or (" X " in currartist.upper()) or ("AND " in currartist.upper()):
+            check_features[currartist] = (100 - i)
+
+        # Add new key if artist not already in the dictionary
+        elif currartist not in weeklypoints:
+            weeklypoints[currartist] = (100 - i)
+
+        # Add to existing artist
+        else:
+            weeklypoints[currartist] += (100 - i)
+    
+
+    # Run the feature_split() function and assign its returned tuple to "features"
+    features = feature_split(weeklypoints, check_features)
+
+    # Assign the dictionary of updated points
+    newpoints = features[0]
+
+    # Assign the dictionary of half-credit features
+    halfCreditFeatures = features[1]
+
+    # Assign the dictionary of full-credit features
+    fullCreditFeatures = features[2]
+
+    # Assign the dictionary of comma flagged entries
+    tylercheck = features[3]
+
+    # Return the 6 dictionaries
+    return [weeklypoints, check_features, newpoints, halfCreditFeatures, fullCreditFeatures, tylercheck]
+
+
 def feature_split(weeklypoints, check_features):
 
     ''' This function splits the features and cobillings into independent entries for easy adding of points. Features are designated if they contain one of 
@@ -119,6 +187,7 @@ def feature_split(weeklypoints, check_features):
     # Return 
     return copiedpoints, halfCreditFeatures, fullCreditFeatures, tylercheck
 
+
 def printdata(chartdata):
     ''' This function prints the data to the terminal for easy reading. if DEBUG is set to True, additional data will be printed.'''
     
@@ -159,73 +228,6 @@ def printdata(chartdata):
     print(f"\nTyler Check")
     print(tylercheck)
 
-
-def create_data():
-    '''Initializes "chart" - an object generated using the Billboard scraper containing the chart data of the Billboard Hot-100. This data is then processed into points. Number 1 is worth 100, number 2 - 99, etc...
-    Different dictionaries are created containing various groups of data and returned
-
-    Args:
-        None
-
-    Returns:
-        weeklypoints: A dictionary containing the artist and points values from entries that contain no features of any kind
-
-        check_features: A dictionary containing artist and point values from entries that contain features. These point values have not been distributed or weighted yet
-
-        newpoints: A dictionary containing artist and points values with points from top billing features added. None of the featured artists have received credit for their secondary appearances
-
-        halfCreditFeatures: A dictionary containing artists and their half-point totals based on true features. These need to be added manually to the total.
-
-        fullCreditFeatures: A dictionary containing artists and their full-point totals based on co-billings. These also need to be manually added.
-
-        tylerCheck: A dictionary containing chart entries that contain a feature keyword AND a comma. This will filter out songs with more than one featured artist, or Tyler, the Creator and his stupid comma-containing stage name.
-
-    '''
-
-    # Create Chart
-    chart = billboard.ChartData('hot-100')
-
-    # Create Dictionary
-    weeklypoints = {}
-    check_features = {}
-
-    # Loop through chart
-    for i in range(len(chart)):
-        currsong = chart[i]
-
-        # Convert artist name to uppercase for easier parsing
-        currartist = str(currsong.artist).upper()
-
-        # Sort out features
-        if ("&" in currartist) or (" WITH " in currartist.upper()) or ("FEATURING " in currartist.upper()) or (" X " in currartist.upper()) or ("AND " in currartist.upper()):
-            check_features[currartist] = (100 - i)
-
-        # Add new key if artist not already in the dictionary
-        elif currartist not in weeklypoints:
-            weeklypoints[currartist] = (100 - i)
-
-        # Add to existing artist
-        else:
-            weeklypoints[currartist] += (100 - i)
-    
-
-    # Run the feature_split() function and assign its returned tuple to "features"
-    features = feature_split(weeklypoints, check_features)
-
-    # Assign the dictionary of updated points
-    newpoints = features[0]
-
-    # Assign the dictionary of half-credit features
-    halfCreditFeatures = features[1]
-
-    # Assign the dictionary of full-credit features
-    fullCreditFeatures = features[2]
-
-    # Assign the dictionary of comma flagged entries
-    tylercheck = features[3]
-
-    # Return the 6 dictionaries
-    return [weeklypoints, check_features, newpoints, halfCreditFeatures, fullCreditFeatures, tylercheck]
 
 def main():
 
